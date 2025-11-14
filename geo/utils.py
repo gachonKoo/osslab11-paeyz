5d33a1c73291c866069786067967119880bd9220
paeyz <iwasbaeyz@gmail.com>
2025-11-14 12:58:40 +0900

    Lab11-geo

--- geo/utils.py
+++ geo/utils.py
@@ -1,14 +1,11 @@
 import math
 
 def distance(x1, y1, x2, y2):
-    """두 점 사이 거리 계산"""
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def area_triangle(base, height):
-    """삼각형 넓이 계산"""
     return 0.5 * base * height
 
 def circle_area(radius):
-    """원의 넓이 계산"""
-    return math.pi * radius**2
+    return math.pi * radius ** 2
 
--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from geo import distance, area_triangle, circle_area
 
-print("distance:", distance(0, 0, 3, 4))
-print("triangle:", area_triangle(10, 5))
-print("circle:", circle_area(3))
+print(distance(0, 0, 3, 4))
+print(area_triangle(10, 5))
+print(circle_area(3))
 
