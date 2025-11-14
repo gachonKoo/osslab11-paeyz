374857217dae2317d1b3bc5e3a8c7d3027679174
paeyz <iwasbaeyz@gmail.com>
2025-11-14 13:27:17 +0900

    lab11-geo

--- tester.py
+++ tester.py
@@ -1,13 +1,7 @@
 from geo import utils
 
-# 거리: (0,0) → (3,4)
-c = utils.distance(0, 0, 3, 4)
-print(f"c = {c}")
+# 1. 거리 (0,0) → (3,4) = 5.0
+print(f"c = {utils.distance(0, 0, 3, 4)}")
 
-# 삼각형 넓이: 밑변 10, 높이 5
-area = utils.area_triangle(10, 5)
-print(f"area = {area}")
-
-# 원 넓이: 반지름 10 → π*10² = 314.159...
-area = utils.circle_area(10)
-print(f"area = {area}")
+# 2. 원의 넓이 (반지름 10) = π*10²
+print(f"area = {utils.circle_area(10)}")
